98ae9a36b0e8982487e191f4c4bdf71b48677100
chinaa <jhonswai@gmail.com>
2023-02-18 16:46:28 +0300

    loader

---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+	return (
+		<div className='w-100 mt-20'>
+			<progress className='progress w-56'></progress>
+		</div>
+	);
+}
+
+export default Loading;
