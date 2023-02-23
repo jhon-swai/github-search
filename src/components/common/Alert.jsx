25b45bdb1cc4724b1408a647bcfc2179f37b8a22
chinaa <jhonswai@gmail.com>
2023-02-23 15:54:56 +0300

    alert component

--- src/components/common/Alert.jsx
+++ src/components/common/Alert.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
 
+import { CiCircleAlert } from 'react-icons/ci';
 function Alert() {
-  return (
-    <div>Alert</div>
-  )
+	const { alert: message } = useContext(AlertContext);
+	return (
+		<div>
+			{message !== null && (
+				<p className='flex items-start mb-4 space-x-2'>
+					{message.type === 'error' && (<CiCircleAlert size={30} color='red'/>)}
+					<p className='flex-1 text-base leading-7 text-red-500'>
+						<strong>{message.msg}</strong>
+					</p>
+				</p>
+			)}
+		</div>
+	);
 }
 
-export default Alert
+export default Alert;
