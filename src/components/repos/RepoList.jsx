00cf756c8594ac10b9f89f6dea14b39a8676f649
Chinaa Swai <jhonswai@gmail.com>
2023-03-10 17:07:21 +0300

    added repository item

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
 
 function RepoList({ repos }) {
 	return (
@@ -7,7 +8,7 @@ function RepoList({ repos }) {
 			<div className='card-body'>
 				<h2 className='text-3xl my-4 font-bold card-title'>Top Repositories</h2>
 				{repos.map((repo) => (
-					<h3>{repo.name}</h3>
+					<RepoItem key={repo.id} repo={repo} />
 				))}
 			</div>
 		</div>
